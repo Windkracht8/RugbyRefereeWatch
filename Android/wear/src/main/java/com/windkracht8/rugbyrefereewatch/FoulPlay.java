98bbcad77c39e858247e7bb2680d4b6681d46ac0
Windkracht8 <dev@windkracht8.com>
2024-03-12 16:11:22 +0100

    Add gradle.properties

--- Android/wear/src/main/java/com/windkracht8/rugbyrefereewatch/FoulPlay.java
+++ Android/wear/src/main/java/com/windkracht8/rugbyrefereewatch/FoulPlay.java
@@ -18,8 +18,7 @@ public class FoulPlay extends LinearLayout{
         assert inflater != null;
         inflater.inflate(R.layout.foulplay, this, true);
         foulPlay_player = findViewById(R.id.foulPlay_player);
-        String[] aPlayerNumbers = new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50"};
-        ArrayAdapter<String> aaPlayerNumbers = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, aPlayerNumbers);
+        ArrayAdapter<String> aaPlayerNumbers = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, Score.aPlayerNumbers);
         aaPlayerNumbers.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         foulPlay_player.setAdapter(aaPlayerNumbers);
         foulPlay_player.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
--- Android/wear/src/main/java/com/windkracht8/rugbyrefereewatch/Score.java
+++ Android/wear/src/main/java/com/windkracht8/rugbyrefereewatch/Score.java
@@ -11,6 +11,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Score extends LinearLayout{
+    static final String[] aPlayerNumbers = new String[] {
+            "0","1","2","3","4","5","6","7","8","9","10"
+            ,"11","12","13","14","15","16","17","18","19","20"
+            ,"21","22","23","24","25","26","27","28","29","30"
+            ,"31","32","33","34","35","36","37","38","39","40"
+            ,"41","42","43","44","45","46","47","48","49","50"
+    };
     MatchData.team team;
     int player_no;
 
@@ -32,7 +39,6 @@ public class Score extends LinearLayout{
         score_goal = findViewById(R.id.score_goal);
         foul_play = findViewById(R.id.foul_play);
 
-        String[] aPlayerNumbers = new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50"};
         ArrayAdapter<String> aaPlayerNumbers = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, aPlayerNumbers);
         aaPlayerNumbers.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         score_player.setAdapter(aaPlayerNumbers);
